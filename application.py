965275c8293f6aa496ed9870e3d52def1eeea680
Yaswanth JK <39457010+Yaswanth3277@users.noreply.github.com>
2021-06-30 21:02:49 -0500

    update templates

--- application.py
+++ application.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@application.route('/searchword', methods=['GET', 'POST'])
+def search_word():
+    return render_template("search_word.html")
+
+
 if __name__ == '__main__':
     application.run()
