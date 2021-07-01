67d7f0fec35cf3d32463c9b7e4f5d437a6531384
Yaswanth JK <39457010+Yaswanth3277@users.noreply.github.com>
2021-06-30 22:34:11 -0500

    working update

--- application.py
+++ application.py
@@ -25,6 +25,10 @@ def search_word():
     word3 = []
     word4 = []
     files = []
+    wordss1 = []
+    wordss2 = []
+    wordss3 = []
+    wordss4 = []
 
     if request.method == 'POST':
         query_word = request.form.get('words')
@@ -32,10 +36,13 @@ def search_word():
         with open(words1, 'rb') as fileinput:
             for line in fileinput:
                 for words in line.split():
-                    word1.append(words)
-        print(word1)
+                    word1.append(str(words))
+        for values in word1:
+            wordss1.append(values[2:-1])
 
-        if query_word in word1:
+        print(wordss1)
+
+        if query_word in wordss1:
             movie_title1 = "AliceCleaneredit.txt"
             files.append(movie_title1)
 
@@ -44,7 +51,12 @@ def search_word():
                 for words in line.split():
                     word2.append(words)
 
-        if query_word in word2:
+        for values in word2:
+            wordss2.append(values[2:-1])
+
+        print(wordss2)
+
+        if query_word in wordss2:
             movie_title2 = "AliceInWonderlandedit.txt"
             files.append(movie_title2)
 
@@ -53,7 +65,12 @@ def search_word():
                 for words in line.split():
                     word3.append(words)
 
-        if query_word in word3:
+        for values in word3:
+            wordss3.append(values[2:-1])
+
+        print(wordss3)
+
+        if query_word in wordss3:
             movie_title3 = "CandideEnedit.txt"
             files.append(movie_title3)
 
@@ -62,7 +79,12 @@ def search_word():
                 for words in line.split():
                     word4.append(words)
 
-        if query_word in word4:
+        for values in word4:
+            wordss4.append(values[2:-1])
+
+        print(wordss4)
+
+        if query_word in wordss4:
             movie_title4 = "CandideFredit.txt"
             files.append(movie_title4)
 
