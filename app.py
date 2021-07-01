0e24a3dddaf363f0e637f4b4eab5f42c0e0234cf
Yaswanth JK <39457010+Yaswanth3277@users.noreply.github.com>
2021-06-30 20:41:47 -0500

    should work

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
 
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 
-@application.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    application.run()
+    app.run()
