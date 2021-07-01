6e4c2a52af518a893793a93230d8f1d9a637f497
Yaswanth JK <39457010+Yaswanth3277@users.noreply.github.com>
2021-06-30 21:14:56 -0500

    search update

--- application.py
+++ application.py
@@ -11,7 +11,30 @@ def index():
 
 @application.route('/searchword', methods=['GET', 'POST'])
 def search_word():
-    return render_template("search_word.html")
+    word1 = []
+    word2 = []
+    word3 = []
+    word4 = []
+    with open('static/AliceCleaneredit.txt', 'r', encoding="utf-8-sig") as fileinput:
+        for line in fileinput:
+            for words in line.split():
+                word1.append(words)
+
+    with open('static/AliceInWonderlandedit.txt', 'r', encoding="utf-8-sig") as fileinput:
+        for line in fileinput:
+            for words in line.split():
+                word2.append(words)
+
+    with open('static/CandideEnedit.txt', 'r', encoding="utf-8-sig") as fileinput:
+        for line in fileinput:
+            for words in line.split():
+                word3.append(words)
+
+    with open('static/CandideFredit.txt', 'r', encoding="utf-8-sig") as fileinput:
+        for line in fileinput:
+            for words in line.split():
+                word4.append(words)
+    return render_template("search_word.html", word1 = word1, word2 = word2, word3 = word3, word4 = word4)
 
 
 if __name__ == '__main__':
